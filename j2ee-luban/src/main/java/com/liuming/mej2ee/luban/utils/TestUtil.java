aab82abae4c35005e03c34a53e47163679bfc140
liuming <liuming_it@sina.com>
2021-03-05 15:10:46 +0800

    luban

---
+++ j2ee-luban/src/main/java/com/liuming/mej2ee/luban/utils/TestUtil.java
@@ -0,0 +1,16 @@
+package com.liuming.mej2ee.luban.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liuming
+ * Date: 2017/9/8
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+
+public class TestUtil {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
